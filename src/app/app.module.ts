f9a4409a6ee46d5fc7912765d882c9575c83bdaa
Anastasiya <32430829+AnastasiyaDidych@users.noreply.github.com>
2018-05-15 19:11:30 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HallComponent } from './hall/hall.component';
+import { HallComponent } from './hall/hallcomp/hall.component';
 
 @NgModule({
   declarations: [
