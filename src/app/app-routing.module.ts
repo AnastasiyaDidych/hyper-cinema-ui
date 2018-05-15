3ec9601a24cc6a98ecfedac05d15719d4d773c6b
VR-Java <vitaliy.rolsky@gmail.com>
2018-05-15 19:16:20 +0300

    refactorind tickets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import { TicketComponent } from './components/ticket/ticket.component';
-import { UserComponent } from './components/user/user.component';
-import { OrderComponent } from './components/order/order.component';
-import { OrderDetailsComponent } from './components/order/order-details/order-details.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { OrderComponent } from './order/order.component';
+import { OrderDetailsComponent } from './order/order-details/order-details.component';
 
 const routes: Routes = [
     {path: 'tickets', component: TicketComponent},
-    {path: 'users', component: UserComponent},
     {path: 'orders', component: OrderComponent},
     {path: 'orders/:id', component: OrderDetailsComponent}
-    // {path: 'dashboard', component: DashboardComponent},
-    // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    // {path: 'detail/:id', component: HeroDetailComponent}
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
-
 import { AppComponent } from './app.component';
-import { TicketModule } from './components/ticket/ticket.module';
-import { SessionModule } from './components/session/session.module';
-import { OrderModule } from './components/order/order.module';
-import { CoefficientModule } from './components/coefficient/coefficient.module';
-import { UserModule } from './components/user/user.module';
-
+import { TicketModule } from './ticket/ticket.module';
+import { OrderModule } from './order/order.module';
 
 import {
   MatButtonModule,
@@ -20,7 +15,7 @@ import {
   MatTableModule
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OrderDetailsModule } from './components/order/order-details/order-details.module';
+import { OrderDetailsModule } from './order/order-details/order-details.module';
 
 
 @NgModule({
@@ -32,10 +27,6 @@ import { OrderDetailsModule } from './components/order/order-details/order-detai
     AppRoutingModule,
 
     TicketModule,
-    SessionModule,
-    OrderModule,
-    CoefficientModule,
-    UserModule,
     OrderModule,
     OrderDetailsModule,
 
