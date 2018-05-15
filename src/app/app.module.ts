d62376e3c96954981b67958e42e23451979fe5ed
Anastasiya <32430829+AnastasiyaDidych@users.noreply.github.com>
2018-05-15 19:09:48 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HallComponent } from './hall/hall.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HallComponent
   ],
   imports: [
     BrowserModule
