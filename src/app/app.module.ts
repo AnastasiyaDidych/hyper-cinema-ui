9f5b34507b7f1357845fea9d2a1c5ce4e7d9406f
wasssabi <nazarlucholat@gmail.com>
2018-05-15 19:24:16 +0300

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {CustomMaterialModule} from './core/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 
-import {AppRoutingModule} from './core/app.routing.module';
-import { LoginComponent } from './login/login.component';
-import {ErrorDialogComponent} from './core/error-dialog.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthService} from "./core/auth.service";
-import {Interceptor} from "./core/inteceptor";
-import {TokenStorage} from "./core/token.storage";
-import {SessionService} from "./shared/session.service";
-import { SessionListComponent } from './session-list/session-list.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
-import { SessionEditComponent } from './session-edit/session-edit.component';
 import { RouterModule, Routes } from '@angular/router';
-import { DisplaySessionComponent } from './display-session/display-session.component';
-import { SearchPipe } from './session-list/search.pipe';
-import { FilterPipe } from './session-list/filter.pipe';
 import { DatePipe } from '@angular/common';
-import { UniquePipe } from './session-list/unique.pipe';
-import { ASD } from './session-list/asd.pipe';
+import { SessionListComponent } from './sessions/session-list/session-list.component';
+import { DisplaySessionComponent } from './sessions/display-session/display-session.component';
+import { SessionEditComponent } from './sessions/session-edit/session-edit.component';
+import { LoginComponent } from './sessions/login/login.component';
+import { ErrorDialogComponent } from './sessions/core/error-dialog.component';
+import { SearchPipe } from './sessions/session-list/search.pipe';
+import { FilterPipe } from './sessions/session-list/filter.pipe';
+import { UniquePipe } from './sessions/session-list/unique.pipe';
+import { ASD } from './sessions/session-list/asd.pipe';
+import { CustomMaterialModule } from './sessions/core/material.module';
+import { AppRoutingModule } from './sessions/core/app.routing.module';
+import { AuthService } from './sessions/core/auth.service';
+import { SessionService } from './sessions/shared/session.service';
+import { TokenStorage } from './sessions/core/token.storage';
+import { Interceptor } from './sessions/core/inteceptor';
 
 
 
