823b1cd887d1947da64fda31e8ddff7a82e597f6
wasssabi <nazarlucholat@gmail.com>
2018-05-17 15:58:15 +0300

    schedule

--- src/app/sessions/session-edit/session.model.ts
+++ src/app/sessions/session-edit/session.model.ts
@@ -1,8 +1,8 @@
 export class Session {
-    id : number;
-    date: string;
-    hallId: number;
-    movieId: number;
-    startTime: string;
-    virtualActive: boolean;
-  }
+  id : number;
+  date: string;
+  hallId: number;
+  movieId: number;
+  startTime: string;
+  virtualActive: boolean;
+}
