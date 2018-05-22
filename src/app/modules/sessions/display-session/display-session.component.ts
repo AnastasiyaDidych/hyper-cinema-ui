e721e9fa09104003327bb8bd556c9e15a8fe7d92
Anastasiya Didych <didychanastasiya@gmail.com>
2018-05-22 16:48:16 +0300

    update DisplaySessionComp

--- src/app/modules/sessions/display-session/display-session.component.ts
+++ src/app/modules/sessions/display-session/display-session.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {SessionService} from '../shared/session.service'
+import { SessionService } from '../shared/session.service'
 import { Session } from '../session-edit/session.model';
 import { ActivatedRoute } from '@angular/router';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { TicketForSession } from '../../hall/model/tictetForSession.model';
+import { Seat } from '../../hall/model/seat.model';
+import { Hall } from '../../hall/model/hall.model';
+import { HallService } from '../../hall/hall.service';
+import { seatArrayInStorage } from '../../hall/hall.component';
 
 export const sessionInStorage = "sessionInStorage";
+export const ticketsArray: Array<TicketForSession> = this.ticketsFromSession;
 
 @Component({
   selector: 'app-display-session',
@@ -13,11 +19,15 @@ export const sessionInStorage = "sessionInStorage";
   styleUrls: ['./display-session.component.css']
 })
 export class DisplaySessionComponent implements OnInit {
-  sessions:Session[] = [];
-  session:Session;
-  id :number;
+  sessions: Session[] = [];
+  session: Session;
+  id: number;
   sub: Subscription;
-  constructor(private sessionService: SessionService,private router: Router,private route: ActivatedRoute) { }
+  ticketsFromSession: Array<TicketForSession> = [];
+  virtuaSeat: Array<Seat> = [];
+  hall: Hall = Object();
+
+  constructor(private hallService: HallService, private sessionService: SessionService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
@@ -27,31 +37,45 @@ export class DisplaySessionComponent implements OnInit {
         this.sessionService.get(id).subscribe((car: any) => {
           if (car) {
             this.session = car;
-            console.log(car);
+            for (var i = 1; i < this.session.tickets.length; i++) {
+              this.ticketsFromSession.push(this.session.tickets[i]);
+            }
+            this.getHall(this.session.hallId);
           } else {
             console.log(`Car with id '${id}' not found, returning to list`);
-            
+
           }
         });
       }
     });
-
-
-    
-  
-    
   }
-  public  generateForDay(session: Session): void {
-    if (window.confirm('Delete ticket?')) {
-    this.sessionService.genetateForDay(session.id)
-      .subscribe(data => {
-        this.sessions = this.sessions.filter(s => s !== session);
-      })
-  }}
 
-  public setSessionToLocalStorage(session: DisplaySessionComponent){
+  public generateForDay(session: Session): void {
+    if (window.confirm('Delete ticket?')) {
+      this.sessionService.genetateForDay(session.id)
+        .subscribe(data => {
+          this.sessions = this.sessions.filter(s => s !== session);
+        })
+    }
+  }
+
+  public setSessionToLocalStorage(session: DisplaySessionComponent) {
     localStorage.setItem(sessionInStorage, JSON.stringify(session));
 
+  }
+
+  public setVirtualSeatToSeatArray() {
+    localStorage.setItem(seatArrayInStorage, JSON.stringify(this.virtuaSeat));
+  }
+
+  public getHall(hall_id: number) {
+    this.hallService.getHall(hall_id).subscribe(data => {
+      this.hall = data;
+      if (this.hall.seats !== null) {
+        this.virtuaSeat.push(this.hall.seats[0]);
+        this.virtuaSeat[0].price = (this.session.vipPrice + 100);
+      }
+    });
   }
 
 }
